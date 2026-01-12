159f4781bdcd026ad224cc8c28ed62d1564e8b85
JAuxance <auxance.jourdan@proton.me>
2026-01-12 15:51:35 +0100

    Format code for better readability in 3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 skip = {113, 101}
-print("{}".format(''.join(chr(i) for i in range(97, 123) if i not in skip)), end='')
+print("{}".format(''.join(chr(i) 
+for i in range(97, 123) if i not in skip)), end='')

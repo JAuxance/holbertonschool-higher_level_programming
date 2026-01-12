e7c708f7ce30e37098d09027f72c5be54757a3bf
JAuxance <auxance.jourdan@proton.me>
2026-01-12 15:58:37 +0100

    Refactor alphabet printing logic for clarity and readability in 3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 skip = {113, 101}
-print("{}".format(''.join(chr(i) 
-for i in range(97, 123) if i not in skip)), end='')
+i = 0
+
+chars = [chr(i) for i in range(97, 123) if i not in skip]
+print("".join(chars))

1b66bde5b5fc7651cc7b716f3cc679221c50583c
JAuxance <auxance.jourdan@proton.me>
2026-01-12 15:32:19 +0100

    Fix condition for last digit evaluation in 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ if number < 0:
 
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif last_digit < 6:
+elif last_digit < 0:
      print(f"Last digit of {number} is {last_digit} and "
           "is 0")
 else:

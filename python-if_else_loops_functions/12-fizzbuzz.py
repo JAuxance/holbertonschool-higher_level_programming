8f68d207501d77c585ba94554ec98f17fb980158
JAuxance <auxance.jourdan@proton.me>
2026-01-13 16:13:06 +0100

    feat: implement fizzbuzz function to print numbers 1 to 100 with Fizz, Buzz, and FizzBuzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,17 @@
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            output = "FizzBuzz"
+        elif i % 3 == 0:
+            output = "Fizz"
+        elif i % 5 == 0:
+            output = "Buzz"
+        else:
+            output = i
+        
+        
+        if i == 100:
+            print(output)
+        else:
+            print(output, end=" ")
+

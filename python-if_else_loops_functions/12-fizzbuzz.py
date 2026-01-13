841a9c6bc3752f7fe6d0765a7ec7bc3cff1844f4
JAuxance <auxance.jourdan@proton.me>
2026-01-13 16:26:04 +0100

    fix: streamline output handling in fizzbuzz function

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 def fizzbuzz():
+    result = []
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            output = "FizzBuzz"
+            result.append("FizzBuzz")
         elif i % 3 == 0:
-            output = "Fizz"
+            result.append("Fizz")
         elif i % 5 == 0:
-            output = "Buzz"
+            result.append("Buzz")
         else:
-            output = i
-
-        if i == 100:
-            print(output, end="")
-        else:
-            print(output, end=" ")
+            result.append(str(i))
+    print(" ".join(result))
 

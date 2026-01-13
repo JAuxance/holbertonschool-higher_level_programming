65ec164002db733d4fc8cf6d90022cd93b49aade
JAuxance <auxance.jourdan@proton.me>
2026-01-13 16:19:50 +0100

    fix: remove trailing space in output for fizzbuzz function

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -11,7 +11,9 @@ def fizzbuzz():
             output = i
 
         if i == 100:
-            print(output, end=" \n")
+            print(output, end="")
         else:
             print(output, end=" ")
 
+
+fizzbuzz()

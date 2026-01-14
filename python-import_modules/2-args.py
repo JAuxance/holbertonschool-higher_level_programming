38eacc625dc343eb6c3c04525405da672ed673f0
JAuxance <auxance.jourdan@proton.me>
2026-01-14 13:12:14 +0100

    fix: move argument count checks inside main guard for proper execution

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,12 +2,13 @@
 import sys
 
 count = len(sys.argv) - 1
-if count == 0:
-    print("{} arguments.".format(count))
-elif count == 1:
-    print("{} argument:".format(count))
-elif count > 1:
-    print("{} arguments:".format(count))
+if __name__ == "__main__":
+    if count == 0:
+        print("{} arguments.".format(count))
+    elif count == 1:
+        print("{} argument:".format(count))
+    elif count > 1:
+        print("{} arguments:".format(count))
 
 for i in range(1, count + 1):
     print("{}: {}".format(i, sys.argv[i]))

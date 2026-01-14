a02f6edb1583f2871169b37107fcd291bdc81fac
JAuxance <auxance.jourdan@proton.me>
2026-01-14 13:10:19 +0100

    feat: add script to display command line arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+count = len(sys.argv) - 1
+if count == 0:
+    print("{} arguments.".format(count))
+elif count == 1:
+    print("{} argument:".format(count))
+elif count > 1:
+    print("{} arguments:".format(count))
+
+for i in range(1, count + 1):
+    print("{}: {}".format(i, sys.argv[i]))

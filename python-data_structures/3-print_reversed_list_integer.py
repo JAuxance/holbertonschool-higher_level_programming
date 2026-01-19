a189173a6237871cb674c61daab83ba11f061694
JAuxance <auxance.jourdan@proton.me>
2026-01-19 15:34:54 +0100

    fix: remove unnecessary end parameter in print_reversed_list_integer function

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     for k in my_list:
         if k == my_list[-1]:
-            print("{:d}".format(k), end="")
-        else:
             print("{:d}".format(k))

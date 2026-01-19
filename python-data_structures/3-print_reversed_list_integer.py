a65a5567a808e1bd75e11c1fe5043be7d10dafcf
JAuxance <auxance.jourdan@proton.me>
2026-01-19 15:35:19 +0100

    fix: correct condition to check for empty list in print_reversed_list_integer function

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
+    if my_list == 0:
+        return None
     for k in my_list:
         if k == my_list[-1]:
             print("{:d}".format(k))

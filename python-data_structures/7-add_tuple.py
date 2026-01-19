9c913a5154af19437d533a28686d5af91af08710
JAuxance <auxance.jourdan@proton.me>
2026-01-19 20:47:51 +0100

    fix: remove unnecessary whitespace and commented-out code in add_tuple function

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     list_a = list(tuple_a)
     list_b = list(tuple_b)
-    
+
     while len(list_a) < 2:
         list_a.append(0)
 
@@ -11,12 +11,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     result = (list_a[0] + list_b[0], list_a[1] + list_b[1])
     return result
-
-
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))

1b08cbbd4d3167a7183a5a97df558902e354b641
JAuxance <auxance.jourdan@proton.me>
2026-01-19 15:37:24 +0100

    fix: remove unused test code from print_reversed_list_integer function

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -5,7 +5,3 @@ def print_reversed_list_integer(my_list=[]):
         return None
     for k in my_list:
         print("{:d}".format(k))
-
-
-my_list = [1, 2, 3, 4, 5]
-print_reversed_list_integer(my_list)

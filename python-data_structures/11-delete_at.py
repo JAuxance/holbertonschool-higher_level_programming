5afd47bebf35845a2916709ccef55ffb54708375
JAuxance <auxance.jourdan@proton.me>
2026-01-19 21:45:03 +0100

    fix: correct index check in delete_at function to prevent index error

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(my_list) - 1:
         return my_list
     del my_list[idx]
     new_list = my_list
     return new_list
+
+my_list = [1, 2, 3, 4, 5]
+idx = 5
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)

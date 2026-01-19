33c16b8faeaaa224fd645552b4cce0e0ca9d0080
JAuxance <auxance.jourdan@proton.me>
2026-01-19 15:34:06 +0100

    fix: correct logic in print_list_integer function to handle end of list formatting

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for k in my_list:
-        if k == 5:
+        if k == my_list[-1]:
             print("{:d}".format(k), end="")
         else:
-            print("{:d}".format(k))
+            print("{:d}".format(k), end="\n")
+
+
+my_list = [1, 2, 3, 4, 43, 232, 333]
+print_list_integer(my_list)

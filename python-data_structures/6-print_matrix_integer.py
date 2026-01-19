00a4462aa2220c968700ddd5f8288f3b345e7105
JAuxance <auxance.jourdan@proton.me>
2026-01-19 16:53:57 +0100

    fix: remove trailing dollar sign from printed matrix elements

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,4 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         formatted_elem = ["{:d}".format(x) for x in row]
-        print(" ".join(formatted_elem) + "$")
+        print(" ".join(formatted_elem))

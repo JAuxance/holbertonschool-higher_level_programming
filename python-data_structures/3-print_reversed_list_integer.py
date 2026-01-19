071124c2d2bfd6a8fcdfad9b9840ede0d11a050c
JAuxance <auxance.jourdan@proton.me>
2026-01-19 15:36:42 +0100

    fix: remove unnecessary condition and simplify print logic in print_reversed_list_integer function

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
-    if my_list == 0:
-        return None
     for k in my_list:
-        if k == my_list[-1]:
-            print("{:d}".format(k))
+        print("{:d}".format(k))
+
+
+my_list = [1, 2, 3, 4, 5]
+print_reversed_list_integer(my_list)

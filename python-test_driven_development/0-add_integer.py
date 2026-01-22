ae89719d6a31abe2bbf341e246f86e32e007e63e
JAuxance <auxance.jourdan@proton.me>
2026-01-22 15:45:53 +0100

    fix: remove unnecessary blank lines in docstring for add_integer function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,14 +7,14 @@ If the parameters are not integers or floats, it raises a TypeError."""
 
 def add_integer(a, b=98):
     """Add two integers or floats.
-    
+
     Args:
         a: An integer or float to add.
         b: An integer or float to add (default is 98).
-    
+
     Returns:
         The sum of a and b as an integer.
-    
+
     Raises:
         TypeError: If a or b is not an integer or float.
     """

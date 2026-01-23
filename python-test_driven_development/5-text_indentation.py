8c6a5a08a8e4d1c981d42d91cdf4cc654fc68be8
JAuxance <auxance.jourdan@proton.me>
2026-01-23 12:10:12 +0100

    feat: add text_indentation function and corresponding test module

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    """Prints a text with 2 new lines after each '.', '?', and ':'.
+
+    Args:
+        text (str): The text to be printed.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    length = len(text)
+    while i < length:
+        print(text[i], end='')
+        if text[i] in {'.', '?', ':'}:
+            print('\n')
+            while i + 1 < length and text[i + 1] == ' ':
+                i += 1
+        i += 1

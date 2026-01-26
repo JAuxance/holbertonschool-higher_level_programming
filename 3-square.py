b52ee9448d3733bb78359c35f68edccae3751bc9
JAuxance <auxance.jourdan@proton.me>
2026-01-26 13:25:23 +0100

    feat: implement Square class with property for size and setter validation

--- 3-square.py
+++ 3-square.py
@@ -6,6 +6,22 @@ class Square:
         if size < 0:
             raise TypeError("size must be >= 0")
         self.__size = size
-        
+
     def area(self):
         return self.__size * self.__size
+
+    def get_size(self):
+        return self.size
+
+
+my_square = Square(89)
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+my_square.size = 3
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+try:
+    my_square.size = "5 feet"
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+except Exception as e:
+    print(e)

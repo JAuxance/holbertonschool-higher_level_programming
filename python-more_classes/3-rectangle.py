77c2c1f80bd8c403e287627b8c8059024c0e1e17
JAuxance <auxance.jourdan@proton.me>
2026-01-28 17:53:23 +0100

    feat: add methods to compare rectangles and create squares in Rectangle class

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 class Rectangle:
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
-
 
     @property
     def height(self):
@@ -50,3 +50,19 @@ class Rectangle:
     def __del__(self):
         print(f"Bye rectangle...")
         Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.width * rect_1.height < rect_2.width * rect_2.height:
+            return rect_2
+        else:
+            return rect_1
+
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)
+

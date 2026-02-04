50345d748a13dfcea33d4b6347be8d7a991e6d85
JAuxance <auxance.jourdan@proton.me>
2026-02-04 10:13:33 +0100

    fix: remove extra blank lines in 3-is_kind_of_class.py

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,11 +5,11 @@
 def is_kind_of_class(obj, a_class):
     """
     Checks if obj is an instance of a_class or a subclass of a_class.
-    
+
     Args:
         obj: The object to check.
         a_class: The class to check against.
-    
+
     Returns:
         True if obj is an instance of a_class or a subclass, False otherwise.
     """

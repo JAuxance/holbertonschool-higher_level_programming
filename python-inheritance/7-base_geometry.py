b9e91b87dddcf16822335e109208d035760f8b08
JAuxance <auxance.jourdan@proton.me>
2026-02-04 10:52:38 +0100

    fix: refine integer validation logic in integer_validator method

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -20,7 +20,7 @@ class BaseGeometry:
             TypeError: if value is not an integer
             ValueError: if value is less than or equal to 0
         """
-        if not isinstance(value, int) or isinstance(value, bool):
-            raise TypeError("{} must be an integer".format(name))
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")

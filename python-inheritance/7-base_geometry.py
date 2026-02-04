de3dd6889b3d31bd81095dec521a510d1699d1c5
JAuxance <auxance.jourdan@proton.me>
2026-02-04 10:18:13 +0100

    fix: remove extra blank lines and ensure newline at end of file in 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,18 +4,18 @@
 
 class BaseGeometry:
     """BaseGeometry class with area and integer_validator methods"""
-    
+
     def area(self):
         """Raises an exception indicating area() is not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """Validates that value is a positive integer
-        
+
         Args:
             name: name of the value (string)
             value: value to validate
-            
+
         Raises:
             TypeError: if value is not an integer
             ValueError: if value is less than or equal to 0
@@ -24,4 +24,3 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        

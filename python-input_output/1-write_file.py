0000ea09013ae992c17cdc8191fa182cb0c05a37
JAuxance <auxance.jourdan@proton.me>
2026-02-09 14:45:58 +0100

    feat: implement append_write function to append text to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
+    """Writes text to a file.
+
+    Args:
+        filename (str): The name of the file to write to.
+        text (str): The text to write to the file.
+
+    Returns:
+        int: The number of characters written.
+    """
     file = open(filename, "w+")
-    file.write(text)
-    file.close
+    chars_written = file.write(text)
+    file.close()
+    return chars_written

d8ecc172eb5a100cea06df49eb59a892dde257eb
JAuxance <auxance.jourdan@proton.me>
2026-02-09 15:43:51 +0100

    refactor: remove test code from append_write function

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -11,7 +11,3 @@ def append_write(filename="", text=""):
     """
     with open(filename, "a") as file:
         return file.write(text)
-
-
-nb_characters_added = append_write("file_append.txt", "This School is so cool!\n")
-print(nb_characters_added)

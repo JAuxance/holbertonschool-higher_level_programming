8275bdd31d6863a287d91db45299a54bc5eb3a45
JAuxance <auxance.jourdan@proton.me>
2026-02-09 16:14:41 +0100

    feat: add JSON files and load_from_json_file function to handle JSON data

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+
+
+def load_from_json_file(filename):
+    """Loads an object from a JSON file.
+
+    Args:
+        filename (str): The name of the JSON file to load from.
+
+    Returns:
+        object: The Python object loaded from the JSON file.
+    """
+    with open(filename, "r")as f:
+        return json.load(f)

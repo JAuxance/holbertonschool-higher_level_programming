fd0eafb57cd52638658efaf32c6dee63308b592f
JAuxance <auxance.jourdan@proton.me>
2026-02-11 09:52:18 +0100

    fix: remove explicit UTF-8 encoding in read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ def read_file(filename=""):
     Args:
         filename (str): The name of the file to read.
     """
-    with open(filename, "r", encoding="utf-8") as file:
+    with open(filename, "r") as file:
         for line in file:
             print(line, end="")

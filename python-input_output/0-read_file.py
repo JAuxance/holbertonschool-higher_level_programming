af8b8b4f5492e5ba510fde57981c6f26da4e402b
JAuxance <auxance.jourdan@proton.me>
2026-02-11 09:55:42 +0100

    fix: simplify read_file function by removing UTF-8 specification in docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,10 +2,9 @@
 """Module 0-read_file: Reads and prints the content of a text file (UTF8)."""
 def read_file(filename=""):
     """
-    Reads a text file (UTF8) and prints it to stdout
+    Reads a text file and prints it to stdout
     Args:
         filename (str): The name of the file to read.
     """
     with open(filename, "r") as file:
-        for line in file:
-            print(line, end="")
+        print(file.read(), end="")

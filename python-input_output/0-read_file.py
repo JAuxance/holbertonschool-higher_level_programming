4774b737f213361aba6df3c81aaf87c5af0264e0
JAuxance <auxance.jourdan@proton.me>
2026-02-11 09:48:58 +0100

    fix: update docstring and ensure UTF-8 encoding in read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""
+0-read_file.py
+Reads and prints the content of a text file (UTF8).
+"""
 def read_file(filename=""):
-    """Reads a text file and prints its content to stdout.
-
+    """
+    Reads a text file (UTF8) and prints it to stdout
     Args:
         filename (str): The name of the file to read.
     """
-    with open(filename, "r") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         for line in file:
-            print((line), end="")
+            print(line, end="")

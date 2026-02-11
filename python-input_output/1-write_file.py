31a1b78611703bda8d067abae8d28f45b2e7b6bd
JAuxance <auxance.jourdan@proton.me>
2026-02-11 09:57:10 +0100

    fix: update module docstring for clarity and consistency in write_file function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""
-1-write_file.py
-Writes text to a file (UTF8).
-"""
+"""Module 1-write_file: Writes text to a file (UTF8)."""
+
 
 
 def write_file(filename="", text=""):
-    """Writes text to a file.
-
+    """
+    Writes text to a file.
     Args:
         filename (str): The name of the file to write to.
         text (str): The text to write to the file.
-
     Returns:
         int: The number of characters written.
     """

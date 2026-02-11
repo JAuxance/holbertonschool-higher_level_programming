92d611995b37e95e6b0f8bb9b792f7b27a6b5f17
JAuxance <auxance.jourdan@proton.me>
2026-02-11 10:02:45 +0100

    fix: correct return statement in write_file function to return character count

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -13,4 +13,5 @@ def write_file(filename="", text=""):
         int: The number of characters written.
     """
     with open(filename, "w") as file:
-        return file.write(text)
+        file.write(text)
+    return len(text)

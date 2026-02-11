2a51b931d2ed21c77a33f6a7b62b8d999d5ba99e
JAuxance <auxance.jourdan@proton.me>
2026-02-11 09:56:16 +0100

    fix: add blank lines for improved readability in read_file module

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module 0-read_file: Reads and prints the content of a text file (UTF8)."""
+
+
 def read_file(filename=""):
     """
     Reads a text file and prints it to stdout

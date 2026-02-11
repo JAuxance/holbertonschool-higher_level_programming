39c2b1408773e981b775ea8d70137b79fffe263c
JAuxance <auxance.jourdan@proton.me>
2026-02-11 12:25:09 +0100

    fix: correct indentation in to_json method for attribute retrieval

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -31,5 +31,5 @@ class Student:
         if attrs is None:
             return self.__dict__
         if isinstance(attrs, list) and all(isinstance(element, str)for element in attrs):
-                return {key: value for key, value in self.__dict__.items() if key in attrs}
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
         return self.__dict__

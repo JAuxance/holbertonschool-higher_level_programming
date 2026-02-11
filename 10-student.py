b74114e5887b018e5b2921e774ab62d1f1e1c0fb
JAuxance <auxance.jourdan@proton.me>
2026-02-11 12:04:18 +0100

    feat: add Student class with initialization and JSON serialization methods

---
+++ 10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module that defines a Student class."""
+
+
+class Student:
+    """Represent a student with first name, last name, and age."""
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a Student instance.
+
+        Args:
+            first_name (str): First name of the student.
+            last_name (str): Last name of the student.
+            age (int): Age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of a Student instance.
+
+        Returns:
+            dict: Dictionary containing all attributes.
+        """
+        if attrs is None:
+            return self.__dict__
+        return {key: value for key, value in self.__dict__.items() if key in attrs}

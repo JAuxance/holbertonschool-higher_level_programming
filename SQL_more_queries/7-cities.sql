b863dc8c5ed95da9c95196b578fd2c1b12baf36a
JAuxance <auxance.jourdan:@proton.me>
2026-03-04 10:53:13 +0000

    feat: create cities table with foreign key to states in hbtn_0d_usa

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,4 +2,4 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 -- Creates the table cities in the database hbtn_0d_usa if it does not exist
 USE `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`(`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,`name` VARCHAR(256) NOT NULL, `state_id` INT NOT NULL, FOREIGN KEY (`state_id`) REFERENCES `states`(`id`)) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`(`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,`state_id` INT NOT NULL, FOREIGN KEY (`state_id`) REFERENCES `states`(`id`), `name` VARCHAR(256) NOT NULL)

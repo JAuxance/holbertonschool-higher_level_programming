cf32678baaba8be3564eedc54ad97e3cc9f49628
JAuxance <auxance.jourdan:@proton.me>
2026-03-04 10:49:20 +0000

    fix : ENGINE

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
 -- Creates the database hbtn_0d_usa if it does not exist
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 -- Creates the table cities in the database hbtn_0d_usa if it does not exist
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`(`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,`name` VARCHAR(256) NOT NULL, `state_id` INT NOT NULL, FOREIGN KEY (`state_id`) REFERENCES `states`(`id`))
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`(`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,`name` VARCHAR(256) NOT NULL, `state_id` INT NOT NULL, FOREIGN KEY (`state_id`) REFERENCES `states`(`id`)) ENGINE=InnoDB;

36cc31ac6cdddbb52794b89d1226ac54c5ba751f
JAuxance <auxance.jourdan:@proton.me>
2026-03-04 10:35:37 +0000

    fix : table now created in USA database

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 -- Creates the database hbtn_0d_usa if it does not exist and creates the table states with an auto-incremented primary key id and a name column.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `states`(`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,`name` VARCHAR(256))
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states`(`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,`name` VARCHAR(256))

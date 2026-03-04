585194cfc84b30c37a45ce40593174d3ea0f79e1
JAuxance <auxance.jourdan:@proton.me>
2026-03-04 10:28:07 +0000

    fix : DEFAULT UNIQUE

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Script that creates the table unique_id on MySQL server.
-CREATE TABLE IF NOT EXISTS `id_not_null` (`id` TINYINT(1), name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS `id_not_null` (`id` INT DEFLAUT 1 UNIQUE, name VARCHAR(256))

58b05aa6a72d55315d69e99a9079ef7484d3822f
JAuxace <auxance.jourdan@proton.me>
2026-03-05 21:48:48 +0100

    Doc: Update docstring and improve SQL query formatting in 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Lists all cities from the database hbtn_0e_4_usa."""
+"""Lists all cities from a database"""
 import MySQLdb
 import sys
-
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
@@ -12,12 +11,17 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
+
     cur = db.cursor()
+
     cur.execute(
-        "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id"
     )
-    rows = cur.fetchall()
-    for row in rows:
+
+    for row in cur.fetchall():
         print(row)
+
     cur.close()
     db.close()

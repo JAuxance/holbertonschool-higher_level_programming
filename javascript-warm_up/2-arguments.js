ea321a0ec0bcc3281f1616f74e6e00ea11b164ea
JAuxace <auxance.jourdan@proton.me>
2026-03-19 20:54:47 +0100

    Chore: Update 2-arguments.js to differentiate between single and multiple arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 const myVar = process.argv[2];
 if (myVar === undefined) {
     console.log('No argument');
-} else {
+} else if (myVar.length > 1) {
     console.log('Arguments found');
+} else {
+    console.log('Argument found');
 }

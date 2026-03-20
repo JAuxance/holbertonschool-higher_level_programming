f5d3be64669ea4d01565b6372a0e6db0fb4f1839
JAuxance <auxance.jourdan@proton.me>
2026-03-20 16:24:54 +0100

    Chore: Improve code readability by formatting while loop in 7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,5 +4,8 @@ let a = 0;
 if (isNaN(xLog)) {
   console.log('Missing number of occurrences');
 } else {
-  while (a < xLog) { a = a + 1, console.log('C is fun'); }
+  while (a < xLog) {
+    console.log('C is fun');
+    a = a + 1;
+  }
 }

eb7713d7288257137b3ca16b1a256283ed1a0b10
JAuxance <auxance.jourdan@proton.me>
2026-03-20 16:45:51 +0100

    Chore: Add 8-square.js script to print a square of 'x' based on user input size

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 const xLog = parseInt(process.argv[2]);
 let a = 0;
 if (isNaN(xLog)) {
-  console.log('Missing number of occurrences');
+    console.log('Missing number of occurrences');
 } else {
-  while (a < xLog) {
-    console.log('C is fun');
-    a = a + 1;
-  }
+    while (a < xLog) {
+        console.log('C is fun');
+        a = a + 1;
+    }
 }
---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+let a = 0;
+let b = 0;
+let xCreator = [];
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  while (a < size) {
+    xCreator = xCreator + ['x'];
+    a = a + 1;
+  }
+  while (b < size) {
+    console.log(xCreator);
+    b = b + 1;
+  }
+}

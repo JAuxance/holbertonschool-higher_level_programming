8c8b66909dde36001b1f0d26a9217f01c8f865e1
JAuxance <auxance.jourdan@proton.me>
2026-03-20 16:16:59 +0100

    Chore: Add 7-multi_c.js script to print 'C is fun' a specified number of times

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let x_log = parseInt(process.argv[2])
+let a = 0
+if (isNaN(x_log)) {
+    console.log("Missing number of occurrences")
+}
+else while (a < x_log)
+    a = a + 1, console.log("C is fun")

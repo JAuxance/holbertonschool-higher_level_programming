8861c923b20370b73b33af8ed892931925381283
JAuxance <auxance.jourdan@proton.me>
2026-03-20 16:17:27 +0100

    Chore: Update 7-multi_c.js to improve code formatting and use const for x_log

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let x_log = parseInt(process.argv[2])
-let a = 0
+const x_log = parseInt(process.argv[2]);
+let a = 0;
 if (isNaN(x_log)) {
-    console.log("Missing number of occurrences")
+  console.log('Missing number of occurrences');
+} else {
+  while (a < x_log) { a = a + 1, console.log('C is fun'); }
 }
-else while (a < x_log)
-    a = a + 1, console.log("C is fun")

46a6e4d41a9f6780e601d7fc61e10fd6ff31611b
JAuxace <auxance.jourdan@proton.me>
2026-03-23 08:26:21 +0100

    Chore: Add HTML structure and JavaScript for header color change on click

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,5 +1,6 @@
 const redHeader = document.querySelector('#red_header');
 const header = document.querySelector('header');
+
 redHeader.addEventListener('click', function () {
-  header.style.color = '#FF0000';
+    header.classList.add('red');
 });

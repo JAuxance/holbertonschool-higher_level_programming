ab8d1c06220a08f1135a8e8aadb984c16116045a
JAuxace <auxance.jourdan@proton.me>
2026-03-23 09:05:59 +0100

    Chore: Refactor JavaScript and HTML files for improved structure and functionality

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -2,5 +2,5 @@ const redHeader = document.querySelector('#red_header');
 const header = document.querySelector('header');
 
 redHeader.addEventListener('click', function () {
-    header.classList.add('red');
+  header.classList.add('red');
 });

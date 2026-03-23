8a0ed58855a8f71acdd968a7d8b0f651e84438ef
JAuxace <auxance.jourdan@proton.me>
2026-03-23 09:29:12 +0100

    Chore: Update file permissions for HTML and JavaScript files; add new HTML and JS files for greeting functionality

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -2,11 +2,11 @@ const listMovies = document.querySelector('#list_movies');
 const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
 
 fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        data.results.forEach(movie => {
-            const li = document.createElement('li');
-            li.textContent = movie.title;
-            listMovies.appendChild(li);
-        });
+  .then(response => response.json())
+  .then(data => {
+    data.results.forEach(movie => {
+      const li = document.createElement('li');
+      li.textContent = movie.title;
+      listMovies.appendChild(li);
     });
+  });

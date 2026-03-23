608428bbf91f9d244cef945aa2c0a1b07cefafe5
JAuxace <auxance.jourdan@proton.me>
2026-03-23 09:21:40 +0100

    Chore: Add HTML and JavaScript files for displaying Star Wars movies

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,7 +1,7 @@
 const character = document.querySelector('#character');
 const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
 fetch(url)
-    .then(Response => Response.json())
-    .then(data => {
-        character.textContent = data.name;
-    });
+  .then(Response => Response.json())
+  .then(data => {
+    character.textContent = data.name;
+  });

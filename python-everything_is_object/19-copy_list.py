90847778e09705034edd9feffc9042be6f2d89af
JAuxance <auxance.jourdan@proton.me>
2026-04-03 09:54:37 +0200

    Fix : Delete test

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -3,14 +3,3 @@ def copy_list(a_list):
     for item in a_list:
         copy_list.append(item)
     return copy_list
-
-my_list = [1, 2, 3]
-print(my_list)
-
-new_list = copy_list(my_list)
-
-print(my_list)
-print(new_list)
-
-print(new_list == my_list)
-print(new_list is my_list)
